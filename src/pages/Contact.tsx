40656ca217e6ec24b9550a905060afb3082ef068
vishnu prajapati <vishnup@serviots.com>
2025-08-07 11:58:07 +0530

    remove commented code

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,19 +1,6 @@
 import React, { useEffect } from "react";
 
 const Contact = () => {
-  useEffect(() => {
-    const handlePopState = (event) => {
-      window.history.pushState(null, "", window.location.href);
-    };
-
-    window.history.pushState(null, "", window.location.href);
-    window.addEventListener("popstate", handlePopState);
-
-    return () => {
-      window.removeEventListener("popstate", handlePopState);
-    };
-  }, []);
-
   return (
     <div>
       <h2>Contact</h2>
